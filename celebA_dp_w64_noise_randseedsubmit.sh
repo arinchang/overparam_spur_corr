fa5debfcb55fed7417e8278214780e9ac64c0eeb
Arin Chang <arinchang@berkeley.edu>
2023-05-30 23:16:47 -0700

    add slurm sample scripts

---
+++ celebA_dp_w64_noise_randseedsubmit.sh
@@ -0,0 +1,47 @@
+# sbatch celebA_dp_w64_noise03_seed1.sh
+# sbatch celebA_dp_w64_noise03_seed2.sh
+# sbatch celebA_dp_w64_noise03_seed3.sh
+
+sbatch celebA_dp_w64_noise035_seed1.sh
+sbatch celebA_dp_w64_noise035_seed2.sh
+sbatch celebA_dp_w64_noise035_seed3.sh
+
+# sbatch celebA_dp_w64_noise04_seed1.sh
+# sbatch celebA_dp_w64_noise04_seed2.sh
+# sbatch celebA_dp_w64_noise04_seed3.sh
+
+
+# sbatch celebA_dp_w64_noise05_seed1.sh
+# sbatch celebA_dp_w64_noise05_seed2.sh
+# sbatch celebA_dp_w64_noise05_seed3.sh
+# sbatch celebA_dp_w64_noise08_seed1.sh
+# sbatch celebA_dp_w64_noise08_seed2.sh
+# sbatch celebA_dp_w64_noise08_seed3.sh
+# sbatch celebA_dp_w64_noise11_seed1.sh
+# sbatch celebA_dp_w64_noise11_seed2.sh
+# sbatch celebA_dp_w64_noise11_seed3.sh
+
+# sbatch celebA_dp_w64_noise14_seed1.sh
+# sbatch celebA_dp_w64_noise14_seed2.sh
+# sbatch celebA_dp_w64_noise14_seed3.sh
+# sbatch celebA_dp_w64_noise17_seed1.sh
+# sbatch celebA_dp_w64_noise17_seed2.sh
+# sbatch celebA_dp_w64_noise17_seed3.sh
+# sbatch celebA_dp_w64_noise20_seed1.sh
+# sbatch celebA_dp_w64_noise20_seed2.sh
+# sbatch celebA_dp_w64_noise20_seed3.sh
+# sbatch celebA_dp_w64_noise23_seed1.sh
+# sbatch celebA_dp_w64_noise23_seed2.sh
+# sbatch celebA_dp_w64_noise23_seed3.sh
+
+
+# sbatch celebA_dp_w64_noise26_seed1.sh
+# sbatch celebA_dp_w64_noise26_seed2.sh
+# sbatch celebA_dp_w64_noise26_seed3.sh
+
+# sbatch celebA_dp_w64_noise29_seed1.sh
+# sbatch celebA_dp_w64_noise29_seed2.sh
+# sbatch celebA_dp_w64_noise29_seed3.sh
+# sbatch celebA_dp_w64_noise32_seed1.sh
+# sbatch celebA_dp_w64_noise32_seed2.sh
+# sbatch celebA_dp_w64_noise32_seed3.sh
---
+++ celebA_erm_submit.sh
@@ -0,0 +1,6 @@
+sbatch celebA_wd0001_erm_1.sh
+sbatch celebA_wd0001_erm_4.sh
+sbatch celebA_wd0001_erm_16.sh
+sbatch celebA_wd0001_erm_32.sh
+sbatch celebA_wd0001_erm_64.sh
+sbatch celebA_wd0001_erm_96.sh
